0e8dec2378453469f327b7fa6c4b2ff41c4acd93
kSushmithak <sushmithasheettyy@gmail.com>
2026-05-07 16:28:32 +0530

    changed terraform file

---
+++ variables.tf
@@ -0,0 +1,21 @@
+variable "resource_group_name" {
+  description = "Name of the Azure Resource Group to create"
+  type        = string
+  default     = "rg-demo-github-actions"
+}
+
+variable "location" {
+  description = "Azure region"
+  type        = string
+  default     = "eastus2"
+}
+
+variable "tags" {
+  description = "Tags applied to the resource group"
+  type        = map(string)
+  default = {
+    environment = "demo"
+    managed_by  = "terraform"
+    project     = "github-actions-demo"
+  }
+}
